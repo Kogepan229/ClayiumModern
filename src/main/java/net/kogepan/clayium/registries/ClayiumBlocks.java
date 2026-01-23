cf76459b43c08fae94d4bf08f108c5623ce31842
Kogepan229 <kogepan25629@gmail.com>
2026-01-23 11:43:10 +0900

    Set block rarity and buffers lang

--- src/main/java/net/kogepan/clayium/registries/ClayiumBlocks.java
+++ src/main/java/net/kogepan/clayium/registries/ClayiumBlocks.java
@@ -6,6 +6,8 @@ import net.kogepan.clayium.blocks.ClayContainerBlock;
 import net.kogepan.clayium.blocks.ClayOre;
 import net.kogepan.clayium.blocks.ClayWorkTableBlock;
 
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -14,6 +16,7 @@ import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMaps;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class ClayiumBlocks {
@@ -23,6 +26,22 @@ public class ClayiumBlocks {
     private static <B extends Block> DeferredBlock<B> register(String name, Supplier<? extends B> sup) {
         DeferredBlock<B> block = BLOCKS.register(name, sup);
         ClayiumItems.ITEMS.registerSimpleBlockItem(block);
+
+        return block;
+    }
+
+    private static <B extends Block> DeferredBlock<B> registerTiered(String name, final int tier,
+                                                                     Function<Integer, ? extends B> func) {
+        assert tier >= 0;
+        DeferredBlock<B> block = BLOCKS.register(name + "_" + tier, () -> func.apply(tier));
+
+        Item.Properties properties = new Item.Properties();
+        switch (tier) {
+            case 4, 5, 6, 7 -> properties.rarity(Rarity.UNCOMMON);
+            case 8, 9, 10, 11 -> properties.rarity(Rarity.RARE);
+            case 12, 13 -> properties.rarity(Rarity.EPIC);
+        }
+        ClayiumItems.ITEMS.registerSimpleBlockItem(block, properties);
 
         return block;
     }
@@ -39,8 +58,7 @@ public class ClayiumBlocks {
     static {
         Int2ObjectMap<DeferredBlock<ClayContainerBlock>> map = new Int2ObjectOpenHashMap<>();
         for (int i = 1; i <= 9; i++) {
-            int finalI = i;
-            map.put(i, register("bending_machine_" + i, () -> new ClayContainerBlock(finalI)));
+            map.put(i, registerTiered("bending_machine", i, ClayContainerBlock::new));
         }
         BENDING_MACHINE_BLOCKS = Int2ObjectMaps.unmodifiable(map);
     }
@@ -49,8 +67,7 @@ public class ClayiumBlocks {
     static {
         Int2ObjectMap<DeferredBlock<ClayBufferBlock>> map = new Int2ObjectOpenHashMap<>();
         for (int i = 4; i <= 13; i++) {
-            int finalI = i;
-            map.put(i, register("clay_buffer_" + i, () -> new ClayBufferBlock(finalI)));
+            map.put(i, registerTiered("clay_buffer", i, ClayBufferBlock::new));
         }
         CLAY_BUFFERS = Int2ObjectMaps.unmodifiable(map);
     }
